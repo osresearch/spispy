d018510f92b4fe46fb53885db74f41ec63700f77
Trammell hudson <hudson@trmm.net>
2019-07-02 13:00:21 +0200

    fifo: fix first-word-fall-through and update test to use read as a ready flag, not a strobe

--- fifo-test.v
+++ fifo-test.v
@@ -11,6 +11,8 @@ module top();
 	reg reset;
 
 	initial begin
+		$dumpfile("fifo-test.vcd");
+		$dumpvars(0,top);
 		clk = 0;
 		reset = 1;
 		repeat(4) #5 clk = ~clk;
@@ -21,14 +23,10 @@ module top();
 	wire space_available, data_available;
 	wire [7:0] read_data;
 	reg [7:0] write_data;
-	reg read_strobe;
+	wire read_strobe;
 	reg write_strobe;
 
-	wire [7:0] write_ptr;
-	wire [7:0] read_ptr;
-	wire [7:0] fifo_count;
-
-	fifo #(.NUM(4)) fifo_i(
+	fifo #(.NUM(16), .FREESPACE(2)) fifo_i(
 		.clk(clk),
 		.reset(reset),
 		.space_available(space_available),
@@ -36,11 +34,7 @@ module top();
 		.write_strobe(write_strobe),
 		.data_available(data_available),
 		.read_data(read_data),
-		.read_strobe(read_strobe),
-		// debug port
-		.write_ptr(write_ptr),
-		.read_ptr(read_ptr),
-		.count(fifo_count)
+		.read_strobe(read_strobe)
 	);
 	
 	always @(posedge clk)
@@ -57,7 +51,7 @@ module top();
 
 		if (reset) begin
 			counter <= 8'h0f;
-			write_data <= 0;
+			write_data <= 8'hA0 - 1;
 		end else begin
 			counter <= counter + 1;
 			if (counter[3:0] < counter[7:4])
@@ -75,29 +69,42 @@ module top();
 		end
 	end
 
+	reg [7:0] expected;
+	reg [3:0] spin;
+
+	assign read_strobe = data_available && spin == 0;
 
 	always @(posedge clk)
 	begin
-		read_strobe <= 0;
-
 		if (reset) begin
+			expected <= 8'hA0;
+			spin <= 0;
+		end else
+		if (spin) begin
+			// burn some cycles
+			spin <= spin - 1;
 		end else
 		if (data_available)
 		begin
-			$display("read: %02x", read_data);
-			read_strobe <= 1;
+			if (read_data != expected)
+				$display("BAD read: %02x != expected %02x", read_data, expected);
+			else
+				$display("read: %02x", read_data);
+			expected <= expected + 1;
+
+			if (expected[1:0] == 2'b11)
+				spin <= ~0;
 		end
 	end
 
 	always
 	begin
 		#5
-		$display("%b write(%b,%02x) read(%b,%02x) avail=%b space=%b %02x %02x %02x",
+		$display("%b write(%b,%02x) read(%b,%02x) avail=%b space=%b",
 			clk,
 			write_strobe, write_data,
 			read_strobe, read_data,
-			data_available, space_available,
-			write_ptr, read_ptr, fifo_count
+			data_available, space_available
 		);
 	end
 endmodule
--- fifo.v
+++ fifo.v
@@ -4,144 +4,63 @@
 `include "util.v"
 
 /*
- * Same clock domain FIFO.
+ * Same clock domain FIFO with first-word-fallthrough.
  *
  * Holds up to NUM entries, which should be a power of two.
  * Can accept a new item every clock cycle,
  * Can export an item every clock cycle.
- * Items will live at least one clock cycle in the ring
- * and become visible the next clock cycle.
+ *
  */
 
 module fifo(
 	input clk,
 	input reset,
-	output data_available,
+	// write side
 	output space_available,
 	input [WIDTH-1:0] write_data,
 	input write_strobe,
+	// read side
+	output data_available,
+	output more_available,
 	output [WIDTH-1:0] read_data,
-	input read_strobe,
-
-	// debugging ports
-	output reg werror,
-	output reg rerror,
-	output [BITS-1:0] write_ptr,
-	output [BITS-1:0] read_ptr,
-	output [BITS-1:0] count
+	input read_strobe
 );
 	parameter WIDTH = 8;
 	parameter NUM = 256;
 	parameter BITS = `CLOG2(NUM);
 	parameter [BITS-1:0] FREESPACE = 1;
 
-	reg [WIDTH-1:0] buffer[0:NUM-1];
-	reg [BITS-1:0] write_ptr;
-	reg [BITS-1:0] read_ptr;
-
-`define simple_test
-`ifdef simple_test
-	reg [BITS-1:0] write_ptr_prev;
-	wire [BITS-1:0] write_ptr_next = write_ptr + write_strobe;
-	wire [BITS-1:0] read_ptr_next = read_ptr + read_strobe;
-	//assign read_data = buffer[read_ptr];
+	reg [BITS:0] count;
+	reg [BITS-1:0] rd_ptr;
+	reg [BITS-1:0] wr_ptr;
 	reg [WIDTH-1:0] read_data;
-	reg [2:0] data_available_delay;
-	assign data_available = !read_strobe && read_ptr_next != write_ptr_prev;
-	assign space_available = write_ptr[BITS-1] == read_ptr[BITS-1]; // {{BITS}{write_ptr_next + 1'h1}} != read_ptr_next;
-	//reg space_available;
+	reg [WIDTH-1:0] ram[0:NUM-1];
 
-	always @(posedge clk)
-	begin
-		if (reset) begin
-			write_ptr <= 0;
-			read_ptr <= 0;
-		end else begin
-			$display("strobe=%b %b data=%02x buf=[%02x,%02x,%02x,%02x]", write_strobe, read_strobe, read_data, buffer[0], buffer[1], buffer[2], buffer[3]);
-
-			if (write_strobe)
-				buffer[write_ptr] <= write_data;
-
-			read_data <= buffer[read_ptr_next];
-
-			write_ptr_prev <= write_ptr;
-			write_ptr <= write_ptr_next;
-			read_ptr <= read_ptr_next;
-
-			//space_available <= {{BITS}{write_ptr + 1'b1}} != read_ptr_next;
-			//data_available <= read_ptr_next != write_ptr;
-		end
-	end
-`else
-	reg [BITS-1:0] count;
-
-/*
-	wire [BITS-1:0] remaining = read_ptr > write_ptr
-			? read_ptr - write_ptr
-			: (NUM-1'b1) - (write_ptr - read_ptr);
-	assign space_available = remaining > FREESPACE;
-	//wire space_available = !data_available;
-	assign space_available = write_ptr + 1'b1 != read_ptr;
-*/
-
-	wire [BITS-1:0] next_read = read_strobe ? read_ptr + 1 : read_ptr;
-	wire [BITS-1:0] next_write = write_strobe ? write_ptr + 1 : write_ptr;
-	wire [BITS-1:0] next_count =
-		read_strobe && !write_strobe ? count - 1 :
-		!read_strobe && write_strobe ? count + 1 :
-		count;
-
-	//reg space_available;
-	//reg data_available;
-	//assign data_available = read_strobe ? 0 : count != 0;
-	assign data_available = next_count != 0;
-	assign space_available = (NUM - 1'b1) - next_count > FREESPACE;
-
-	//assign read_data = write_strobe && data_available ? write_data : buffer[read_ptr];
-	assign read_data = buffer[read_ptr];
-		//count == 0 && next_count == 0 ? write_data : buffer[read_ptr];
-	//reg [WIDTH-1:0] read_data;
+	assign more_available = (count > 1);
+	assign data_available = (count != 0);
+	assign space_available = (NUM - count > FREESPACE);
 
 	always @(posedge clk) begin
 		if (reset) begin
-			write_ptr <= 0;
-			read_ptr <= 0;
-			werror <= 0;
-			rerror <= 0;
+			rd_ptr <= 0;
+			wr_ptr <= 0;
 			count <= 0;
-			//space_available <= 0;
-			//data_available <= 0;
 		end else begin
-			$display("strobe=%b %b data=%02x count=%d next_count=%d buf=[%02x,%02x,%02x,%02x]", write_strobe, read_strobe, read_data, count, next_count, buffer[0], buffer[1], buffer[2], buffer[3]);
 			if (write_strobe)
-				buffer[write_ptr] <= write_data;
-/*
-			if (write_strobe && next_count == 1)
-			begin
-				$display("fast forward write");
+				ram[wr_ptr] <= write_data;
+
+			// first word fall through
+			if ((write_strobe && count == 0)
+			||  (write_strobe && read_strobe && count == 1))
 				read_data <= write_data;
-			end else
-				read_data <= buffer[read_ptr];
-*/
+			else
+				read_data <= ram[rd_ptr + read_strobe];
 
-			// if the fifo is empty, allow the write data to flow directly
-			// to the read data to avoid a clock cycle in the fifo
-			//if (write_strobe && read_ptr == write_ptr)
-				//read_data <= write_data;
-			//else
-				//read_data <= buffer[read_ptr];
-			
-
-			read_ptr <= next_read;
-			write_ptr <= next_write;
-			count <= next_count;
-
-			//data_available <= next_count != 0;
-
-			//read_data <= buffer[next_read];
+			rd_ptr <= rd_ptr + read_strobe;
+			wr_ptr <= wr_ptr + write_strobe;
+			count <= count + write_strobe - read_strobe;
 		end
 	end
-`endif
 endmodule
 
 
