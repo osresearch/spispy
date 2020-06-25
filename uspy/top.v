bb5f0a82ba39d354f4b625dc5e08166ab128f0c8
Trammell hudson <hudson@trmm.net>
2020-06-25 14:23:48 +0200

    uspispy: single spi decoding works at 20 MHz
    Signed-off-by: Trammell hudson <hudson@trmm.net>

--- uspy/top.v
+++ uspy/top.v
@@ -3,10 +3,15 @@
  *
  *               +------+
  *  !CS       ---| o    |----  +V
- *   DO / D0  ---|      |---- !RST / D3 
+ *   DO / D1  ---|      |---- !RST / D3 
  *  !WP / D2  ---|      |----  CLK
- *  GND       ---|      |----  DI / D1
+ *  GND       ---|      |----  DI / D0
  *               +------+
+// teensy3 pins
+#define SPI_CS   10 // white or yellow
+#define SPI_SCLK 13 // green
+#define SPI_MOSI 11 // blue or purple
+#define SPI_MISO 12 // brown
  */
 `default_nettype none
 `include "uart.v"
@@ -22,18 +27,18 @@ module top(
 	output led_b,
 	// left side GPIO
 	inout gpio_23,
-	inout gpio_25, // D1
+	inout gpio_25, // D0 / DI
 	inout gpio_26, // CLK
 	inout gpio_27, // D3
 	inout gpio_32, // D2
-	inout gpio_35, // D0
-	inout gpio_31, // !CS
-	inout gpio_37,
+	//inout gpio_35,
+	//inout gpio_31, // !CS
+	inout gpio_37, // D1 / DO
 	inout gpio_34, // !CS ram0
-	inout gpio_43, // D0 ram0
+	inout gpio_43, // D1 ram0
 	inout gpio_36, // D2 ram0
 	inout gpio_42, // !CS ram1
-	inout gpio_38, // D0 ram1
+	inout gpio_38, // D1 ram1
 	inout gpio_28, // D2 ram1
 
 	// right side GPIO
@@ -41,7 +46,7 @@ module top(
 	inout gpio_21,
 	inout gpio_13,
 	inout gpio_19,
-	inout gpio_18,
+	inout gpio_18, // !CS
 	inout gpio_11,
 	inout gpio_9,
 	inout gpio_6,
@@ -49,27 +54,34 @@ module top(
 	inout gpio_4,
 	inout gpio_3, // D3 ram0
 	inout gpio_48, // clk ram0
-	inout gpio_45, // D1 ram0
+	inout gpio_45, // D0 ram0
 	inout gpio_47, // D3 ram1
 	inout gpio_46, // clk ram1
-	inout gpio_2, // D1 ram1
+	inout gpio_2, // D0 ram1
 );
 	assign fpga_spi_cs = 1;
 
-	wire clk_48;
-	wire clk = clk_48;
-	wire reset = 0;
+	wire clk_48mhz;
 	SB_HFOSC u_hfosc(
 		.CLKHFPU(1),
 		.CLKHFEN(1),
-		.CLKHF(clk_48),
+		.CLKHF(clk_48mhz),
 	);
+
+/*
+	wire locked;
+	wire clk;
+	wire reset = !locked;
+	pll_120 pll(clk_48mhz, clk, locked);
+*/
+	wire clk = clk_48mhz;
+	wire reset = 0;
 
 	assign led_b = serial_rxd;
 	assign led_g = serial_txd;
-	assign led_r = 1; // negative logic
+	assign led_r = spi_cs_in; // negative logic
 
-	// divide clk48 by 16 to get a 3 MHz baud clock
+	// divide clk60 by 20 to get a 3 MHz baud clock
 	wire clk_3;
 	divide_by_n #(16) baud_clock(clk, reset, clk_3);
 	wire [7:0] uart_tx;
@@ -86,16 +98,16 @@ module top(
 
 	// physical pins for the three SPI ports
 	wire spi_clk_pin = gpio_26;
-	wire spi_cs_pin = gpio_31;
-	wire [3:0] spi_data_pins = { gpio_27, gpio_32, gpio_25, gpio_35 };
+	wire spi_cs_pin = gpio_18;
+	wire [3:0] spi_data_pins = { gpio_27, gpio_32, gpio_37, gpio_25 };
 
 	wire ram0_clk_pin = gpio_48;
 	wire ram0_cs_pin = gpio_34;
-	wire [3:0] ram0_data_pins = { gpio_3, gpio_36, gpio_45, gpio_43 };
+	wire [3:0] ram0_data_pins = { gpio_3, gpio_36, gpio_43, gpio_45 };
 
 	wire ram1_clk_pin = gpio_46;
 	wire ram1_cs_pin = gpio_42;
-	wire [3:0] ram1_data_pins = { gpio_47, gpio_28, gpio_2, gpio_38 };
+	wire [3:0] ram1_data_pins = { gpio_47, gpio_28, gpio_38, gpio_2 };
 
 	// bidirectional pins for the spi bus data
 	wire [3:0] spi_di;
@@ -104,23 +116,37 @@ module top(
 	wire spi_cs_enable;
 	wire spi_cs_in;
 	wire spi_cs_out;
+	wire spi_clk;
 
 	SB_IO #(
-		.PIN_TYPE(6'b1010_01) // tristatable outputs
+		.PIN_TYPE(6'b1010_01), // tristatable outputs
+		//.PULLUP(4'b1111),
 	) spi_data_buffer[3:0] (
 		.OUTPUT_ENABLE(spi_do_enable),
 		.PACKAGE_PIN(spi_data_pins),
 		.D_IN_0(spi_di),
 		.D_OUT_0(spi_do),
 	);
+
 	SB_IO #(
-		.PIN_TYPE(6'b1010_01) // tristatable outputs
+		.PIN_TYPE(6'b1010_01), // tristatable outputs
+		//.PULLUP(1'b1),
 	) spi_cs_buffer (
 		.OUTPUT_ENABLE(spi_cs_enable),
 		.PACKAGE_PIN(spi_cs_pin),
 		.D_IN_0(spi_cs_in),
 		.D_OUT_0(spi_cs_out),
 	);
+
+	// unidirectional for the clk pin
+	SB_IO #(
+		.PIN_TYPE(6'b1010_01),
+	) spi_clk_buffer(
+		.PACKAGE_PIN(spi_clk_pin),
+		.OUTPUT_ENABLE(0),
+		.D_IN_0(spi_clk),
+	);
+
 
 	// bidirectional pins for the ram0 data
 	wire [3:0] ram0_di;
@@ -153,9 +179,9 @@ module top(
 		.reset(reset),
 		.uart_tx(uart_tx),
 		.uart_tx_strobe(uart_tx_strobe),
-		.spi_clk(spi_clk_pin),
-		.spi_cs_i(spi_cs_in),
-		.spi_cs_o(spi_cs_out),
+		.spi_clk(spi_clk),
+		.spi_cs_in(spi_cs_in),
+		.spi_cs_out(spi_cs_out),
 		.spi_cs_enable(spi_cs_enable),
 		.spi_di(spi_di),
 		.spi_do(spi_do),
--- uspy/uspispy.v
+++ uspy/uspispy.v
@@ -8,6 +8,7 @@
 `define _uspispy_v_
 
 `include "qspi.v"
+`include "util.v"
 
 module uspispy(
 	input clk,
@@ -23,8 +24,8 @@ module uspispy(
 	// this involves driving the !CS pin sometimes, so it has a output enable pin
 	// the data pins are bidirectional
 	input spi_clk,
-	input spi_cs_i,
-	input spi_cs_o,
+	input spi_cs_in,
+	input spi_cs_out,
 	output spi_cs_enable,
 	input [3:0] spi_di,
 	output [3:0] spi_do,
@@ -46,17 +47,17 @@ module uspispy(
 	output [3:0] ram1_do_enable,
 );
 	// copy the CS and CLK from the SPI bus to the RAM chips
-	assign spi_cs_o = spi_cs_i;
-	assign ram0_cs = spi_cs_i;
-	assign ram1_cs = spi_cs_i;
+	assign spi_cs_out = spi_cs_in;
+	assign ram0_cs = spi_cs_in;
+	assign ram1_cs = spi_cs_in;
 	assign ram0_clk = spi_clk;
 	assign ram1_clk = spi_clk;
 
 	// these will need some adjustment
 	assign spi_cs_enable = 0;
-	assign spi_do_enable = 4'b0000; // No output driven
-	assign ram0_do_enable = 4'b0001; // DO pin to ram0
-	assign ram1_do_enable = 4'b0001; // DO pin to ram1
+	reg [3:0] spi_do_enable = 0;
+	assign ram0_do_enable = 4'b0001; // DI pin from the PCH is the DO pin TO ram0
+	assign ram1_do_enable = 4'b0001; // DI pin from the PCH is the DO pin TO ram1
 	assign ram0_do = spi_di;
 	assign ram1_do = spi_di;
 
@@ -65,22 +66,31 @@ module uspispy(
 	wire [7:0] spi_byte;
 	reg [7:0] spi_cmd;
 	reg [7:0] spi_phase;
-	reg [7:0] spi_byte_out = 0;
+	reg [7:0] spi_byte_out = 8'hA5;
 
 	reg [7:0] uart_tx;
 	reg uart_tx_strobe;
+
+	// select which output goes to the PCH
+	wire output_fpga = 1;
+	wire output_ram = 0;
+	wire [3:0] spi_do_fpga;
+	assign spi_do = output_fpga ? spi_do_fpga : output_ram ? ram0_di : ram1_di;
+
+	wire spi_cs_sync;
 
 	qspi_sync qspi(
 		.clk(clk),
 		.reset(reset),
 		// physical interface
-		.spi_cs_in(spi_cs_i),
+		.spi_cs_in(spi_cs_in),
+		.spi_cs_out(spi_cs_sync),
 		.spi_clk_in(spi_clk),
 		.spi_data_in(spi_di),
-		.spi_data_out(spi_do),
+		.spi_data_out(spi_do_fpga),
 		// logical interface
-		.byte_strobe(spi_byte_strobe),
-		.start_strobe(spi_start_strobe),
+		.spi_byte_strobe(spi_byte_strobe),
+		.spi_cmd_strobe(spi_start_strobe),
 		.byte(spi_byte),
 		.byte_tx(spi_byte_out)
 	);
@@ -89,23 +99,28 @@ module uspispy(
 	begin
 		uart_tx_strobe <= 0;
 
-		if (spi_cs_i || reset) begin
-			// nothing to do unless selected
-		end else
-		if (!spi_byte_strobe) begin
-			// nothing to do; wait for a command to start
+		if (spi_cs_sync || reset) begin
+			// nothing to do and no output drivers unless selected
+			spi_do_enable <= 0;
+			spi_phase <= 0;
 		end else
 		if (spi_start_strobe) begin
 			// new command!
 			uart_tx_strobe <= 1;
-			uart_tx <= spi_byte;
+			uart_tx <= "A"; //spi_byte;
 			spi_cmd <= spi_byte;
-			spi_phase <= 0; // start of new command
-		end else begin
+
+			// enable our data out pin TO the pch
+			// todo: update spi_do_enable based on the single/dual/quad mode
+			spi_do_enable <= 4'b0010;
+		end else
+		if (spi_byte_strobe) begin
 			// accumulate command bytes
 			spi_phase <= spi_phase + 1;
 			uart_tx_strobe <= 1;
+			uart_tx <= `hexdigit(spi_phase); // spi_byte;
 			uart_tx <= spi_byte;
+			spi_byte_out <= spi_byte_out + 1;
 		end
 	end
 endmodule
