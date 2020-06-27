61002e1623390f40732b3fd50e21a4daadfffca4
Trammell hudson <hudson@trmm.net>
2020-06-27 18:42:20 +0200

    uspi: write steering works (although NOR mapping does not yet)
    Signed-off-by: Trammell hudson <hudson@trmm.net>

--- uspy/uspispy.v
+++ uspy/uspispy.v
@@ -19,6 +19,31 @@ Supported commands:
 `include "qspi.v"
 `include "util.v"
 
+module mem(
+	input wr_clk,
+	input wr_enable,
+	input [7:0] wr_addr,
+	input [7:0] wr_data,
+	input rd_clk,
+	input [7:0] rd_addr,
+	output [7:0] rd_data
+);
+	reg [7:0] ram[0:255];
+	reg [7:0] rd_data;
+
+	always @(posedge wr_clk)
+	begin
+		if (wr_enable)
+			ram[wr_addr] <= wr_data;
+	end
+
+	always @(posedge rd_clk)
+	begin
+		rd_data <= ram[rd_addr];
+	end
+endmodule
+
+
 module uspispy(
 	input clk,
 	input reset,
@@ -56,9 +81,13 @@ module uspispy(
 	output [3:0] ram1_do_enable,
 );
 	// copy the CS and CLK from the SPI bus to the RAM chips
+	// most of them time the commands will be sent to both of them,
+	// with the exception of the write commands that must be filtered.
 	assign spi_cs_out = spi_cs_in;
-	assign ram0_cs = spi_cs_in;
-	assign ram1_cs = spi_cs_in;
+	reg ram0_enabled = 1;
+	reg ram1_enabled = 1;
+	assign ram0_cs = spi_cs_in || !ram0_enabled; // negative logic
+	assign ram1_cs = spi_cs_in || !ram1_enabled; // negative logic
 	assign ram0_clk = spi_clk;
 	assign ram1_clk = spi_clk;
 
@@ -68,6 +97,7 @@ module uspispy(
 	localparam SPI_CMD_WREN = 8'h06;
 	localparam SPI_CMD_WRDS = 8'h04;
 	localparam SPI_CMD_READ = 8'h03;
+	localparam SPI_CMD_PP3	= 8'h02;
 
 	// these will need some adjustment
 	assign spi_cs_enable = 0;
@@ -90,8 +120,9 @@ module uspispy(
 
 	// select which output goes to the PCH
 	reg read_in_progress = 0;
+	reg write_in_progress = 0;
 	wire output_fpga = !read_in_progress;
-	wire output_ram = read_addr[23]; // select which RAM based on the top bit
+	wire output_ram = spi_addr[23]; // select which RAM based on the top bit
 	wire [3:0] spi_do_fpga;
 	assign spi_do = output_fpga ? spi_do_fpga : output_ram ? ram1_di : ram0_di;
 
@@ -118,17 +149,37 @@ module uspispy(
 	reg [2:0] spi_sr_bp = 0;
 	reg spi_sr_wel = 0;
 	reg spi_sr_busy = 0;
-	wire [7:0] spi_sr = { spi_sr_srp0, spi_sr_sec, spi_sr_tb, spi_sr_bp, spi_sr_wel, spi_sr_busy };
+	wire [7:0] spi_sr = {
+		spi_sr_srp0,
+		spi_sr_sec,
+		spi_sr_tb,
+		spi_sr_bp,
+		spi_sr_wel,
+		spi_sr_busy
+	};
 	wire [23:0] spi_id = 24'hC22018;
 
-	reg [23:0] read_addr = 0;
+	reg [23:0] spi_addr = 0;
+
+	reg write_buffer_enable = 0;
+	reg [7:0] write_offset = 0;
+	reg [7:0] read_offset = 0;
+	wire [7:0] read_data;
+	mem write_buffer(spi_clk, write_buffer_enable, write_offset, spi_byte_raw, clk, read_offset, read_data);
 
 	always @(posedge spi_clk or posedge spi_cs_in)
 	begin
+		write_buffer_enable <= 0;
+
 		if (spi_cs_in) begin
 			// nothing to do, turn off any output drivers
 			spi_do_enable <= 4'b0000;
 			read_in_progress <= 0;
+			write_in_progress <= 0;
+
+			// select both ram chips most of the time
+			ram1_enabled <= 1;
+			ram0_enabled <= 1;
 		end else
 		if (spi_cmd_strobe_raw) begin
 			spi_cmd <= spi_byte_raw;
@@ -156,6 +207,11 @@ module uspispy(
 				// the RAM chips, not the fpga
 				read_in_progress <= 1;
 			end
+			SPI_CMD_PP3: begin
+				// a write has started -- start buffering it
+				write_in_progress <= 1;
+				write_offset <= 0;
+			end
 			default: begin
 				// unknown command, do not respond
 			end
@@ -168,6 +224,11 @@ module uspispy(
 			if (spi_phase != 3)
 				spi_phase <= spi_phase + 1;
 
+			// assume that the first three bytes are the address
+			if (spi_phase == 0) spi_addr[23:16] <= spi_byte_raw;
+			if (spi_phase == 1) spi_addr[15: 8] <= spi_byte_raw;
+			if (spi_phase == 2) spi_addr[ 7: 0] <= spi_byte_raw;
+
 			case (spi_cmd)
 			SPI_CMD_RDID: begin
 				if (spi_phase == 0)
@@ -177,19 +238,27 @@ module uspispy(
 					spi_byte_out <= spi_id[7:0];
 			end
 			SPI_CMD_READ: begin
-				if (spi_phase == 0) begin
-					read_addr[23:16] <= spi_byte_raw;
-				end else
-				if (spi_phase == 1) begin
-					read_addr[15:8] <= spi_byte_raw;
-				end else
 				if (spi_phase == 2) begin
-					read_addr[7:0] <= spi_byte_raw;
-					spi_do_enable <= 4'b0010; // from the RAM chips
+					// enable our output pin, which will be routed
+					// from the appropriate RAM chip
+					spi_do_enable <= 4'b0010;
 				end else
-				begin
+				if (spi_phase == 3) begin
 					// update the read address for each byte that is strobed
-					read_addr <= read_addr + 1;
+					spi_addr <= spi_addr + 1;
+				end
+			end
+			SPI_CMD_PP3: begin
+				if (spi_phase == 0) begin
+					// select the correct RAM chip based on the top bit
+					// of the address.
+					ram0_enabled <= spi_byte_raw[7] == 0;
+					ram1_enabled <= spi_byte_raw[7] == 1;
+				end else
+				if (spi_phase == 3) begin
+					// store the byte into the write buffer
+					write_buffer_enable <= 1;
+					write_offset <= write_offset + 1;
 				end
 			end
 			default: begin
